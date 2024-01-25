b4c4fcdc52ac73e1e021aa718330a43811f6c6ce
Subhadeep <subhadeepsarkar1995@gmail.com>
2024-01-25 20:58:56 +0530

    add middleware protected routes are active cannot access login and register route if already logged in cannot access profile page and redirected to login if logged out

---
+++ auth/src/middleware.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname
+  const isPublicPath = path === "/auth/login" || path === "/auth/register"
+
+  const token = request.cookies.get("token")?.value || ""
+
+  if (isPublicPath && token) {
+    return NextResponse.redirect(new URL("/", request.url))
+  }
+
+  if (!isPublicPath && !token) {
+    return NextResponse.redirect(new URL("/auth/login", request.url))
+  }
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ["/about/:path*", "/", "/profile/:path*", "/auth/:path*"],
+}
