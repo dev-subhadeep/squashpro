c10aa0f805aeb3bde9c6049fce65e61630d0a75c
Subhadeep <subhadeepsarkar1995@gmail.com>
2024-01-27 15:53:24 +0530

    local build test check

---
+++ app/dist/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"cgbYUJT1IavwVHjGcSKFTDL30mveqjo28zPef4in6Wo=\"}"
--- app/src/app/chat/page.tsx
+++ app/src/app/chat/page.tsx
@@ -1,19 +1,9 @@
-"use client"
-
-import CurrentChat from "@/components/CurrentChat"
-import MessageDirectory from "@/components/MessageDirectory"
-import MessagesList from "@/components/MessagesList"
-import ChatProvider from "@/context/ChatContext"
 import React from "react"
 
 const ChatPage = () => {
   return (
     <div className="flex flex-row justify-between">
-      <ChatProvider>
-        <MessagesList />
-        <CurrentChat />
-        <MessageDirectory />
-      </ChatProvider>
+      <h1>Chat Page. Coming soon.</h1>
     </div>
   )
 }
